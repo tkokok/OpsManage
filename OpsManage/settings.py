a09b615640e75f5ebef4cc0b98a6004fcb9ea51b
tkokok <ddy2008@tkokok.cn>
2018-07-04 21:23:32 +0800

    update settings

--- OpsManage/settings.py
+++ OpsManage/settings.py
@@ -14,50 +14,48 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 import djcelery
-from celery import  platforms
-from kombu import Queue,Exchange
+from celery import platforms
+from kombu import Queue, Exchange
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 ''' celery config '''
 djcelery.setup_loader()
-BROKER_URL = 'redis://192.168.88.233:6379/4' 
+BROKER_URL = 'redis://192.168.88.233:6379/4'
 CELERY_RESULT_BACKEND = 'djcelery.backends.database.DatabaseBackend'
 CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TASK_SERIALIZER='pickle'
-CELERY_ACCEPT_CONTENT = ['pickle','json']
+CELERY_TASK_SERIALIZER = 'pickle'
+CELERY_ACCEPT_CONTENT = ['pickle', 'json']
 CELERYBEAT_SCHEDULER = 'djcelery.schedulers.DatabaseScheduler'
 CELERY_TASK_RESULT_EXPIRES = 60 * 60 * 24
 CELERYD_MAX_TASKS_PER_CHILD = 40
 CELERY_TRACK_STARTED = True
 CELERY_ENABLE_UTC = False
-CELERY_TIMEZONE='Asia/Shanghai'
+CELERY_TIMEZONE = 'Asia/Shanghai'
 platforms.C_FORCE_ROOT = True
 
-#celery route config
-CELERY_IMPORTS = ("OpsManage.tasks.assets","OpsManage.tasks.ansible",
-                  "OpsManage.tasks.cron","OpsManage.tasks.deploy",
-                  "OpsManage.tasks.sql","OpsManage.tasks.sched")
+# celery route config
+CELERY_IMPORTS = ("OpsManage.tasks.assets", "OpsManage.tasks.ansible",
+                  "OpsManage.tasks.cron", "OpsManage.tasks.deploy",
+                  "OpsManage.tasks.sql", "OpsManage.tasks.sched")
 CELERY_QUEUES = (
-    Queue('default',Exchange('default'),routing_key='default'),
-    Queue('ansible',Exchange('ansible'),routing_key='ansible_#'),
+    Queue('default', Exchange('default'), routing_key='default'),
+    Queue('ansible', Exchange('ansible'), routing_key='ansible_#'),
 )
 CELERY_ROUTES = {
-    'OpsManage.tasks.sql.*':{'queue':'default','routing_key':'default'},
-    'OpsManage.tasks.assets.*':{'queue':'default','routing_key':'default'},
-    'OpsManage.tasks.cron.*':{'queue':'default','routing_key':'default'},
-    'OpsManage.tasks.sched.*':{'queue':'default','routing_key':'default'},
-    'OpsManage.tasks.ansible.AnsibleScripts':{'queue':'ansible','routing_key':'ansible_scripts'},
-    'OpsManage.tasks.ansible.AnsiblePlayBook':{'queue':'ansible','routing_key':'ansible_playbook'},
+    'OpsManage.tasks.sql.*': {'queue': 'default', 'routing_key': 'default'},
+    'OpsManage.tasks.assets.*': {'queue': 'default', 'routing_key': 'default'},
+    'OpsManage.tasks.cron.*': {'queue': 'default', 'routing_key': 'default'},
+    'OpsManage.tasks.sched.*': {'queue': 'default', 'routing_key': 'default'},
+    'OpsManage.tasks.ansible.AnsibleScripts': {'queue': 'ansible', 'routing_key': 'ansible_scripts'},
+    'OpsManage.tasks.ansible.AnsiblePlayBook': {'queue': 'ansible', 'routing_key': 'ansible_playbook'},
 }
 CELERY_DEFAULT_QUEUE = 'default'
 CELERY_DEFAULT_EXCHANGE_TYPE = 'direct'
 CELERY_DEFAULT_ROUTING_KEY = 'default'
 
-
-
-REDSI_KWARGS_LPUSH = {"host":'192.168.88.233','port':6379,'db':3}
+REDSI_KWARGS_LPUSH = {"host": '192.168.88.233', 'port': 6379, 'db': 3}
 REDSI_LPUSH_POOL = None
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -70,21 +68,20 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
 # Channels settings
 CHANNEL_LAYERS = {
     "default": {
-       "BACKEND": "asgi_redis.RedisChannelLayer",  # use redis backend
-       "CONFIG": {
-            "hosts": [("localhost", 6379)],  #无密码方式
+        "BACKEND": "asgi_redis.RedisChannelLayer",  # use redis backend
+        "CONFIG": {
+            "hosts": [("localhost", 6379)],  # 无密码方式
             "channel_capacity": {
-                                   "http.request": 1000,
-                                   "websocket.send*": 10000,
-                                },
-            "capacity": 10000,           
-           },
-       "ROUTING": "OpsManage.routing.channel_routing",  # load routing from our routing.py file
-       },
+                "http.request": 1000,
+                "websocket.send*": 10000,
+            },
+            "capacity": 10000,
+        },
+        "ROUTING": "OpsManage.routing.channel_routing",  # load routing from our routing.py file
+    },
 }
 
 # Application definition
@@ -112,7 +109,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-#     'django.middleware.csrf.CsrfResponseMiddleware',
+    #     'django.middleware.csrf.CsrfResponseMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -127,16 +124,15 @@ REST_FRAMEWORK = {
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
-    ),             
+    ),
 }
-
 
 ROOT_URLCONF = 'OpsManage.urls'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["/mnt/OpsManage/OpsManage/static/",'/mnt/OpsManage/OpsManage/templates/'],
+        'DIRS': ["/mnt/OpsManage/OpsManage/static/", '/mnt/OpsManage/OpsManage/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -149,28 +145,22 @@ TEMPLATES = [
     },
 ]
 
-
 WSGI_APPLICATION = 'OpsManage.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE':'django.db.backends.mysql',
-        'NAME':'opsmanage',
-        'USER':'root',
-        'PASSWORD':'welliam',
-        'HOST':'192.168.88.201'                
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE':'django.db.backends.mysql',
+        # 'NAME':'opsmanage',
+        # 'USER':'root',
+        # 'PASSWORD':'welliam',
+        # 'HOST':'192.168.88.201'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-
-
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
@@ -178,21 +168,19 @@ LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Asia/Shanghai'
 
-
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-     '/mnt/OpsManage/OpsManage/static/',
-    )
+    '/mnt/OpsManage/OpsManage/static/',
+)
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'upload/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'upload/')
 MEDIA_URL = '/upload/'
 
 SFTP_CONF = {
-             'port':22,
-             'username':'root',
-             'password':'welliam',
-             'timeout':30
-             }  #修改成能sftp登陆OpsManage的账户
+    'port': 22,
+    'username': 'root',
+    'password': 'welliam',
+    'timeout': 30
+}  # 修改成能sftp登陆OpsManage的账户
 
 LOGIN_URL = '/login'
-
